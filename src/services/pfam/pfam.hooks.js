6161ba18cc306f575b82b9f38fbf21044d391d98
Han Lin <hotdogee@gmail.com>
2019-10-02 19:47:28 +0800

    added pfamAClans

--- src/services/pfam/pfam.hooks.js
+++ src/services/pfam/pfam.hooks.js
@@ -5,6 +5,7 @@ const debug = require('debug')('pfam.hooks')
 const errors = require('@feathersjs/errors')
 const axios = require('axios')
 const Ajv = require('ajv')
+const pfamAClans = require('../../assets/pfamAClans')
 /* eslint-disable no-unused-vars */
 const {
   validateSchema,
@@ -20,55 +21,6 @@ const {
 
 const metaPath = './src/assets/pfam-regions-d0-s20-meta.json'
 const meta = JSON.parse(fs.readFileSync(metaPath, 'utf8'))
-
-const pfamAClansTsvPath = './src/assets/Pfam-A.clans.tsv'
-const pfamAClans = fs
-  .readFileSync(pfamAClansTsvPath, 'utf8')
-  .trim()
-  .split('\n')
-  .reduce((a, v) => {
-    v = v.split('\t').map((vv) => vv.trim())
-    a[v[0]] = {
-      clanAcc: v[1],
-      clanId: v[2],
-      pfamId: v[3],
-      pfamDesc: v[4]
-    }
-    return a
-  }, {})
-
-// "PAD",
-// "NO_DOMAIN",
-// "UNKNOWN_DOMAIN",
-pfamAClans.PAD = {
-  clanAcc: '',
-  clanId: '',
-  pfamId: 'PAD',
-  pfamDesc: 'Sequence Padding'
-}
-pfamAClans.NO_DOMAIN = {
-  clanAcc: '',
-  clanId: '',
-  pfamId: 'NO_DOMAIN',
-  pfamDesc: 'No Domain'
-}
-pfamAClans.UNKNOWN_DOMAIN = {
-  clanAcc: '',
-  clanId: '',
-  pfamId: 'UNKNOWN_DOMAIN',
-  pfamDesc: 'Unknown Domain'
-}
-// > Object.keys(pfamAClans).length
-// 16712
-// Object.values(pfamAClans).reduce((a, v) => {
-//   for (let [k, vv] of Object.entries(v)) {
-//     if (vv) {
-//       a[k] = a[k] ? a[k] + 1 : 1
-//     }
-//   }
-//   return a
-// }, {})
-// { clanAcc: 5996, clanId: 5996, pfamId: 16712, pfamDesc: 16712 }
 
 const ajv = new Ajv({ allErrors: true, $data: true })
 const createPfamSchema = {
--- src/services/references/references.hooks.js
+++ src/services/references/references.hooks.js
@@ -1,3 +1,58 @@
+const pfamAClans = require('../../assets/pfamAClans')
+
+/* eslint-disable no-unused-vars */
+const debug = require('debug')('references.hooks')
+const {
+  FeathersError,
+  BadRequest,
+  NotAuthenticated,
+  PaymentError,
+  Forbidden,
+  NotFound,
+  MethodNotAllowed,
+  NotAcceptable,
+  Timeout,
+  Conflict,
+  LengthRequired,
+  Unprocessable,
+  TooManyRequests,
+  GeneralError,
+  NotImplemented,
+  BadGateway,
+  Unavailable
+} = require('@feathersjs/errors')
+const {
+  iff,
+  mongoKeys,
+  keep,
+  discard,
+  disallow,
+  isProvider,
+  populate,
+  alterItems,
+  checkContext,
+  paramsFromClient,
+  paramsForServer
+} = require('feathers-hooks-common')
+/* eslint-enables no-unused-vars */
+
+function addPfamInfo () {
+  return async (context) => {
+    // check type === 'after', method === 'find'
+    checkContext(context, 'after', ['find'], 'addPfamInfo')
+    const { result } = context
+    const { data } = result
+    data.forEach((item) => {
+      const pfamAcc = item.pfamAcc
+      item.pfamId = pfamAClans[pfamAcc].pfamId
+      item.pfamDesc = pfamAClans[pfamAcc].pfamDesc
+      item.clanAcc = pfamAClans[pfamAcc].clanAcc
+      item.clanId = pfamAClans[pfamAcc].clanId
+    })
+    return context
+  }
+}
+
 module.exports = {
   before: {
     all: [],
@@ -11,7 +66,7 @@ module.exports = {
 
   after: {
     all: [],
-    find: [],
+    find: [addPfamInfo()],
     get: [],
     create: [],
     update: [],
